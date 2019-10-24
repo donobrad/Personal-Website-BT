8f65ca2fc295c7cbf73e41a3f5c9546ed691fb1f
Donovan Bradley <dbjpk12@gmail.com>
2019-10-23 23:13:43 -0600

    Adding Project

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("a").hover(function () {
+        $(this).toggleClass("logoBig");
+    });
+
+    let githubRepo = new XMLHttpRequest();
+    githubRepo.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let myRepo = JSON.parse(this.responseText);
+            var repo = "";
+            for (i = 0; i < myRepo.length; i++) {
+                repo += "<li>" + "1.  " + myRepo[i].name + "</li>";
+            }
+            document.getElementById("gitHubRepo").innerHTML = repo;
+        }
+    }
+    githubRepo.open("GET", "https://api.github.com/users/donobrad/repos", true);
+    githubRepo.send();
+});
+
+
